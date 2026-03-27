3b098ddeda0edeadb18a9f098c459d1060f315a4
dovlatyanc <dovlatyanc.ru@gmail.com>
2026-03-27 14:26:25 +0300

    вариант готовый к сдаче

--- app/src/main/java/com/example/sky_track_mobile/presentation/screens/FlightsScreen.kt
+++ app/src/main/java/com/example/sky_track_mobile/presentation/screens/FlightsScreen.kt
@@ -1,11 +1,30 @@
 package com.example.sky_track_mobile.presentation.screens
 
 import android.content.Intent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,10 +37,10 @@ import com.example.sky_track_mobile.data.models.Flight
 import com.example.sky_track_mobile.data.repository.FlightRepository
 import com.example.sky_track_mobile.presentation.components.FlightCard
 import com.example.sky_track_mobile.presentation.theme.Sky_track_mobileTheme
-import kotlinx.coroutines.launch
-import org.koin.compose.koinInject
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
